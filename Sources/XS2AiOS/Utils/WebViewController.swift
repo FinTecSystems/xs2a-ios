30381a96a46d2618f4589a93d81122178c653da1
Felix Fritz <ffritz@fintecsystems.com>
2021-11-17 17:14:57 +0100

    Implement icon for secure state in webview.

--- Sources/XS2AiOS/Utils/WebViewController.swift
+++ Sources/XS2AiOS/Utils/WebViewController.swift
@@ -69,6 +69,51 @@ class WebViewController: UIViewController, WKNavigationDelegate, WKScriptMessage
 
 		}
 	}
+	
+	private func getMenuButton(isSecure: Bool) -> UIBarButtonItem {
+		let barButton = UIButton(type: .custom)
+		barButton.frame = CGRect(x: 0.0, y: 0.0, width: 20, height: 20)
+		barButton.setImage(UIImage(named: isSecure ? "lock" : "lock_slash", in: .current, compatibleWith: nil), for: .normal)
+
+		let barButtonItem = UIBarButtonItem(customView: barButton)
+		let currWidth = barButtonItem.customView?.widthAnchor.constraint(equalToConstant: 24)
+		currWidth?.isActive = true
+		let currHeight = barButtonItem.customView?.heightAnchor.constraint(equalToConstant: 24)
+		currHeight?.isActive = true
+		
+		return barButtonItem
+	}
+	
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		/// Check for a secure connection
+		if let serverTrust = self.webView.serverTrust {
+			DispatchQueue.global().async {
+				var isSecure = false
+
+				if #available(iOS 13.0, *) {
+					SecTrustEvaluateAsyncWithError(serverTrust, DispatchQueue.global()) {
+						trust, result, error in
+
+						if result {
+							isSecure = true
+						}
+					}
+				} else {
+					var trustResult: SecTrustResultType = .invalid
+					SecTrustEvaluate(serverTrust, &trustResult)
+
+					if (trustResult == .proceed || trustResult == .unspecified) {
+						isSecure = true
+					}
+				}
+				
+				/// Update UI to reflect security status
+				DispatchQueue.main.async {
+					self.navigationItem.rightBarButtonItem = self.getMenuButton(isSecure: isSecure)
+				}
+			}
+		}
+	}
 
 	@objc func backbuttonPressed() {
 		self.dismiss(animated: true, completion: nil)
