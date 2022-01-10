ae58f62486d07affbb0296a1afbb881ed1ed1e50
Felix Fritz <ffritz@fintecsystems.com>
2022-01-10 11:22:24 +0100

    Version bump to 1.2.2

--- Sources/XS2AiOS/Utils/APIService.swift
+++ Sources/XS2AiOS/Utils/APIService.swift
@@ -96,7 +96,7 @@ class APIService {
 	/// Function for making the initial call to the XS2A backend
 	func initCall(completion: @escaping (APIResponseType) -> Void) {
 		let payload: [String:Any] = [
-			"version": "ios_sdk_1.2.1",
+			"version": "ios_sdk_1.2.2",
 			"client": "ios_sdk",
 		]
 
