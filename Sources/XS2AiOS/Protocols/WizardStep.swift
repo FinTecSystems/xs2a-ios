e2655dfa2066360d6f27b1241ac6ad95b18e9372
Felix Fritz <ffritz@fintecsystems.com>
2022-10-12 16:03:04 +0200

    use basic step enums only

--- Sources/XS2AiOS/Protocols/WizardStep.swift
+++ Sources/XS2AiOS/Protocols/WizardStep.swift
@@ -1,17 +1,6 @@
 public enum WizardStep: String {
 	case tan
 	case bank
-	case accounts
+	case account
 	case login
-	case login_next
-	case finish
-	case data
-	case loading_accounts
-	case sca
-	case intermission
-	case pre_login
-	case pre_accounts
-	case credit_plus
-	case arvato_demo
-	case pre_data
 }
