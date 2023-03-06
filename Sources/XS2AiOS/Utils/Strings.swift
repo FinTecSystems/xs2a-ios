1eb441f3f76fd0a592bb5170be5e23d74777e612
Felix Fritz <felix.fritz@tink.com>
2023-03-06 14:46:12 +0100

    guard bundle and path

--- Sources/XS2AiOS/Utils/Strings.swift
+++ Sources/XS2AiOS/Utils/Strings.swift
@@ -45,9 +45,14 @@ enum Strings {
 
 extension String {
 	static func localized(key: String) -> String {
-		let path = Bundle.current.path(forResource: XS2AiOS.shared.configuration.language.rawValue, ofType: "lproj")
-		let bundle = Bundle(path: path!)
+		guard let path = Bundle.current.path(forResource: XS2AiOS.shared.configuration.language.rawValue, ofType: "lproj") else {
+			fatalError("Bundle path not found for resource \(XS2AiOS.shared.configuration.language.rawValue)")
+		}
+		
+		guard let bundle = Bundle(path: path) else {
+			fatalError("Bundle not found for path \(path)")
+		}
 
-		return NSLocalizedString(key, tableName: nil, bundle: bundle!, value: "", comment: "")
+		return NSLocalizedString(key, tableName: nil, bundle: bundle, value: "", comment: "")
 	}
 }
