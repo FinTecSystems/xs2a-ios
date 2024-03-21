68ec5f66bcb097b907cf2eec62a470c3b1a370a9
Felix Fritz <felix.fritz@tink.com>
2024-03-21 10:32:04 +0100

    version bump

--- Sources/XS2AiOS/Utils/APIService.swift
+++ Sources/XS2AiOS/Utils/APIService.swift
@@ -119,7 +119,7 @@ class APIService {
 	/// Function for making the initial call to the XS2A backend
 	func initCall(completion: @escaping (APIResponseType) -> Void) {
 		var payload: [String:Any] = [
-			"version": "ios_sdk_1.15.4",
+			"version": "ios_sdk_1.15.5",
 			"client": "ios_sdk",
 		]
 		
